046fc029c50306d5f0814f0d1a7dfcc042609c62
kamilamin <kamil.amin@live.co.uk>
2018-04-17 15:41:16 +0500

    Initial Commit

---
+++ src/lottery.js
@@ -0,0 +1,10 @@
+//Instance of web3 library
+import web3 from './web3';
+
+//Address where we deploy our contract
+const address = '0xFdaF15a37068C9D6be803249Fc80D85AEAd516cb';
+
+//ABI of our Contract
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"random","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"listPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
